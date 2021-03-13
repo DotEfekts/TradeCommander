ca38df607f634652af07404318831fac8e2088c0
DotEfekts <jack@dotefekts.net>
2021-03-13 07:34:19 +0000

    Deploying to gh-pages from  @ db1150d1344f94eb48a2577bfe30034f4a6cb128 🚀

--- js/main.js
+++ js/main.js
@@ -1,7 +1,17 @@
 ﻿(function () {
     document.onmouseup = function () {
-        var input = document.getElementById('command-input');
-        if (input)
+        var highlightedText = "";
+        if (window.getSelection) {
+            highlightedText = window.getSelection().toString();
+        }
+        else if (document.selection && document.selection.type !== "Control") {
+            highlightedText = document.selection.createRange().text;
+        }
+
+        if (highlightedText === "") {
+            var input = document.getElementById('command-input');
+            if(input)
             input.focus();
+        }
     }
 })();
